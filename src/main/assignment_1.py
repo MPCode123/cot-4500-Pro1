61249a8dbc1914e2622440dedc5a8c74aea11b15
MPCode123 <mq.pham2003@gmail.com>
2025-01-29 18:03:11 -0500

    Algorithm Part 1
    Completed 2 of 4 algorithms needed

--- src/main/assignment_1.py
+++ src/main/assignment_1.py
@@ -1,1 +1,46 @@
-print("Hello World")
+# Approximation Method
+def Approximate(x0, tol):
+    iter = 0
+    diff = x0
+    x = x0
+    
+    print("This is Approximation Method")
+    print(iter , ": " , x)
+    
+    while (diff >= tol):
+        iter += 1
+        y = x
+        x = (x/2) + (1/x)
+        print(iter, ":" , x)
+        diff = abs(x-y)
+        
+    # print("Reached end of iterations")
+    print("It will converge after %d iterations" %iter)
+
+
+def f(x):
+    return x*x*x + x*x + 2
+
+def Bisect(left, right, max, tol):
+    iter = 0
+    
+    while (abs(right - left) > tol and iter < max):
+        iter += 1
+        p = (left + right) / 2
+        
+        if ((f(left) < 0 and f(p) > 0) or (f(left) > 0 and f(p) < 0)):
+            right = p
+        else:
+            left = p
+            
+    return p
+
+#def FixedPoint(p0, tol, n0):
+    #i = 1
+    
+    #while (i <= n0):
+       # p = 
+    
+        
+    
+    
--- src/test/test_assignment_1.py
+++ src/test/test_assignment_1.py
@@ -1,1 +1,20 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
+from src.main.assignment_1 import *
+
+
+Approximate(1.5, .000001)
+print("\n")
+
+print(Bisect(1,100,5,.000001))
+print("\n")
+
+
+
+
+
+
+
 
